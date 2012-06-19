bec7b7f4c0938895b0bba8f354150916624414b7
sehrish habib <sehrish@gmail.com>
2012-06-19 04:24:56 -0700

    changes made

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,12 +5,35 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-       $this->_redirect('home');
+       
     }
 
     public function indexAction()
     {
-        
+      
+$smtpServer = 'smtp.gmail.com';
+$username = 'habibsehrish@gmail.com';
+$password = 'h8lovestory';
+$config = array(
+        'ssl' => 'ssl',
+                'auth' => 'login',
+                'username' => $username,
+                'password' => $password,
+        'port' => 465
+        );
+$transport = new Zend_Mail_Transport_Smtp($smtpServer, $config);
+Zend_Mail::setDefaultTransport($transport);
+
+$receiver = $_GET['userName'];
+$to = $_GET['email'];
+
+$mail = new Zend_Mail();
+$mail->addTo($to, $receiver);
+$mail->setSubject('you have registered!');
+$mail->setBodyText('Congratulations!Get your account activated');
+$mail->setFrom('habibsehrish@gmail.com', 'Sehrish');
+$mail->send();
+ 
     }
 
 
