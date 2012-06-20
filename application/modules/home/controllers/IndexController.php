35f2ea32a704e62006d07363bee87a02237da55c
muhammad Saqib <rooott@gmail.com>
2012-06-20 14:21:25 +0500

    project files updated

--- application/modules/home/controllers/IndexController.php
+++ application/modules/home/controllers/IndexController.php
@@ -5,8 +5,7 @@ class Home_IndexController extends Zend_Controller_Action
 
     public function init()
     {
-	echo "new Page";      
-  	/* Initialize action controller here */
+	
     }
 
     public function indexAction()
