b73013e606e4cf7a689d89d148d001f77494d6d4
muhammad Saqib <rooott@gmail.com>
2012-06-25 12:52:13 +0500

    some files deleted in modules

--- application/modules/admin/Bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Admin_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-
-
-}
--- application/modules/home/Bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Home_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-
-
-}
