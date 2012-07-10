891863fa13b48b0c5c59a36b7bfbadf6e7b38b84
Muhammad Saqib <saqib.msn@hotmail.com>
2012-07-10 12:31:54 +0500

    contoller changed

--- application/controllers/UactiveController.php
+++ application/controllers/UactiveController.php
@@ -1,3 +1,58 @@
+<<<<<<< HEAD
+<?php
+
+class UactiveController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+
+
+    }
+
+    public function indexAction()
+    {
+    	//Displaying the form
+    	$form = new Application_Form_Uactive();
+    	$this->view->form = $form;
+
+    	 if ($this->getRequest()->isPost() && $this->view->form->isValid($this->_getAllParams()))
+       {
+
+       	    //getting the password from the form and inserting it in the db
+
+            $password = $form->getValue('password');
+            $user = new Application_Model_Users();
+         	  $userName = $this->_request->getParam('userName');
+          	$where = "userName = '$userName'";
+        	  $data = array('password' => sha1($form->getValue('password')));
+        	  $user->update($data, $where);
+
+        	  echo "Your Password has been changed!";
+	     }
+
+	  
+    
+        else
+        {
+              $password = $form->getValue('password');
+              $user = new Application_Model_Users();
+             	$userName = $this->_request->getParam('userName');
+              $where = "userName = '$userName'";
+            	$data = array('password' => sha1($form->getValue('password')));
+            	$user->update($data, $where);
+
+        	    echo "Your Password has been changed!";
+    
+	     }
+
+    }
+
+
+
+}
+=======
 <?php
 
 class UactiveController extends Zend_Controller_Action
@@ -98,3 +153,4 @@ class UactiveController extends Zend_Controller_Action
 
 }
 }
+>>>>>>> 1391e354057ce1ad316cb3a511df5b4dde77c6bb
